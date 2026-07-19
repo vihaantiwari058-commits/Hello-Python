52c5285db6ab27370f3a8417abbdb37798559dfc
vihaan <vihaan.tiwari058@gmail.com>
2026-07-19 14:03:08 +0530

    Add number guessing game

--- hello.py
+++ hello.py
@@ -1,18 +1,23 @@
-num1 = (int(input("enter a number1:")))
-num2 = (int(input("enter a number2:")))
+print("hello World")
+print("Welcome to the number guessing game!")
+import random 
 
-print("enter symbol: + - * / %")
-sign = input("enter a sign ")
+answer = random.randrange(1, 101)
+chances = 0 
+print("You have 10 chances to guess the number between 1 and 100.")
 
-if sign == "+":
-    print(num1 + num2)
-elif sign == "-":
-    print(num1 - num2)
-elif sign == "*":
-    print(num1 * num2)
-elif sign == "/":
-    print(num1 / num2) 
-elif sign == "%":
-    print(num1 % num2)
-else:
-    print("Invalid operator")
+while chances < 10:
+    guess = int(input("Enter your guess: "))
+    chances += 1
+
+    if guess < answer:
+        print("Your guess is too low.")
+    elif guess > answer:
+        print("Your guess is too high.")
+    else:
+        print(f"Congratulations! You've guessed the number {answer} in {chances} chances.")
+        break
+
+    if chances == 10 and guess != answer:
+    print(f"""Sorry, you've used all your chances and failed to guess the number. 
+        The correct number was {answer}.""") 
